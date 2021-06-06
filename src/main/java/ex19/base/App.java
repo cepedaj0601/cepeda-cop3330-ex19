f6a58ec4665f803e989ba868ff3587ea07bff7fd
Justin Laptop <cepedaj0601@gmail.com>
2021-06-06 13:53:59 -0400

    Initial solution

--- src/main/java/ex19/base/App.java
+++ src/main/java/ex19/base/App.java
@@ -40,15 +40,15 @@ public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("What is your weight?");
+        System.out.println("What is your weight (in pounds)?");
         double weight = in.nextDouble();
 
-        System.out.println("What is your height?");
+        System.out.println("What is your height (in inches)?");
         double height = in.nextDouble();
 
         double bmi = (weight / (height * height)) * 703;
 
-        System.out.printf("Your BMI is: %.2f", bmi);
+        System.out.printf("Your BMI is: %.2f\n", bmi);
 
         if (bmi > 25) {
             System.out.println("You are overweight, you should see your doctor.");
